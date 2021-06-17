1dfb868473f67358414fe01736687ec3e2a99107
jy1987 <87jy1015@gmail.com>
2021-06-17 16:07:08 +0900

    Sign Up

--- users/forms.py
+++ users/forms.py
@@ -18,3 +18,37 @@ class LoginForm(forms.Form):
                 self.add_error("password", forms.ValidationError("password is wrong"))
         except models.User.DoesNotExist:
             self.add_error("email", forms.ValidationError("User not exist"))
+
+
+class SignUpForm(forms.Form):
+    first_name = forms.CharField(max_length=50)
+    last_name = forms.CharField(max_length=50)
+    email = forms.EmailField()
+    password = forms.CharField(widget=forms.PasswordInput)
+    password1 = forms.CharField(widget=forms.PasswordInput, label="Confirm PW")
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        try:
+            models.User.objects.get(email=email)
+            raise forms.ValidationError("User already exists")
+        except models.User.DoesNotExist:
+            return email
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password")
+        confirm_pw = self.cleaned_data.get("password1")
+        if password != confirm_pw:
+            raise forms.ValidationError("password are not matched")
+        else:
+            return password
+
+    def save(self):
+        first_name = self.cleaned_data.get("first_name")
+        last_name = self.cleaned_data.get("last_name")
+        email = self.cleaned_data.get("email")
+        password = self.cleaned_data.get("password")
+        user = models.User.objects.create_user(email, email, password)
+        user.first_name=first_name
+        user.last_name=last_name
+        user.save()
--- users/views.py
+++ users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.views import View
+from django.views.generic import FormView
 from django.contrib.auth import authenticate, login, logout
 from . import forms
 
@@ -12,7 +13,7 @@ class LoginView(View):
         form = forms.LoginForm()
         return render(request, "users/login.html", {"form": form})
 
-    def post(sel, request):
+    def post(self, request):
         form = forms.LoginForm(request.POST)
         if form.is_valid():
             email = form.cleaned_data.get("email")
@@ -29,3 +30,19 @@ def log_out(request):
 
     logout(request)
     return redirect(reverse("core:home"))
+
+
+class SignUpView(FormView):
+    template_name = "users/signup.html"
+    form_class = forms.SignUpForm
+    success_url = reverse_lazy("core:home")
+
+    def form_valid(self, form):
+        form.save()
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        user = authenticate(self.request, username=email, password=password)
+        if user is not None:
+            login(self.request, user)
+            return redirect(reverse("core:home"))
+        return super().form_valid(form)
