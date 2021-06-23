794af10353327dcc8c02a5cc22673a4b4be0c519
jy1987 <87jy1015@gmail.com>
2021-06-23 16:14:55 +0900

    Css for Detail.html

--- categories/urls.py
+++ categories/urls.py
@@ -3,4 +3,7 @@ from categories import views as genre_views
 
 app_name = "genre"
 
-urlpatterns = [path("", genre_views.all_genres, name="genre")]
+urlpatterns = [
+    path("", genre_views.all_genres, name="genre"),
+    path("<int:pk>/", genre_views.DetailGenre.as_view(), name="detail"),
+]
--- categories/views.py
+++ categories/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
 from . import models
 
 # Create your views here.
@@ -8,3 +9,10 @@ def all_genres(request):
 
     all_genres = models.Genre.objects.all()
     return render(request, "genres/all_genres.html", context={"genres": all_genres})
+
+
+class DetailGenre(DetailView):
+
+    model = models.Genre
+    context_object_name = "genre"
+    template_name = "genres/genre_detail.html"
